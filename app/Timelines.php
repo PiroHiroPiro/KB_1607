155b26f9d9ce6cbad60dcaeba02305acb68efb6d
Hiro <hirohiro11071107@gmail.com>
2016-11-05 13:15:59 +0900

    make model

---
+++ app/Timelines.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timelines extends Model
+{
+    //
+    protected $table = 'Timelines';
+    protected $guarded = array('id');
+}
