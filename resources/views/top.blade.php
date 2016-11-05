eea509661b01b0208b03f7b775080325161f3c92
Hiro <hirohiro11071107@gmail.com>
2016-11-05 17:56:54 +0900

    add link when pushed content

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,11 +7,13 @@
 @section('content')
     <div class="inner">
         <?php foreach ($Contents as $Content) { ?>
-            <div class="content">
-                <img src="<?php echo $Content->picture1 ?>">
-                <h2><?php echo $Content->title ?></h2>
-                <p><?php echo $Content->name ?></p>
-            </div>
+            <a href="/contents/<?php echo ?>">
+                <div class="content">
+                    <img src="<?php echo $Content->picture1 ?>">
+                    <h2><?php echo $Content->title ?></h2>
+                    <p><?php echo $Content->name ?></p>
+                </div>
+            </a>
         <?php } ?>
     </div>
 @endsection
