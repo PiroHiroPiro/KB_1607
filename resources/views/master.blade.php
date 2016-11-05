17fd22f03ff26b765e687fbdb7ac1c807fd46584
Hiro <hirohiro11071107@gmail.com>
2016-11-06 00:50:47 +0900

    change header source code

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,42 +17,41 @@
 <body>
     <!-- header -->
     <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand logo-around" href="#">
-            <img alt="募金Tech" src="/img/logo-sample.png" id="logo">
-          </a>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand logo-around" href="#">
+                    <img alt="募金Tech" src="/img/logo-sample.png" id="logo">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="menu-list"><a href="/"><i class="fa fa-home"></i> Top</a></li>
+                    <li class="menu-list"><a href="/timeline"><i class="fa fa-th-large"></i> TimeLine</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                    @else
+                        <li id="user-info">
+                            <i class="fa fa-user"></i> ようこそ {{ Auth::user()->name }} さん
+                        </li>
+                    @endif
+                    <li id="log-info">
+                        @if (Auth::guest())
+                            <a href="/auth/login"><i class="fa fa-sign-in"></i> Login</a>
+                        @else
+                            <a href="/auth/logout"><i class="fa fa-sign-out"></i> Logout</a>
+                        @endif
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="menu-list"><a href="/"><i class="fa fa-home"></i> Top</a></li>
-            <li class="menu-list"><a href="/timeline"><i class="fa fa-th-large"></i> TimeLine</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li id="user-info">
-                @if (Auth::guest())
-                @else
-                    <i class="fa fa-user"></i> ようこそ {{ Auth::user()->name }} さん
-                @endif
-            </li>
-            <li id="log-info">
-                @if (Auth::guest())
-                    <a href="/auth/login"><i class="fa fa-sign-in"></i> Login</a>
-                @else
-                    <a href="/auth/logout"><i class="fa fa-sign-out"></i> Logout</a>
-                @endif
-            </li>
-          </ul>
-        </div>
-      </div>
     </nav>
-
 
     <!-- container -->
     <div class="container">
