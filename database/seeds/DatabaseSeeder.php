70a60abcfb3c7bb4873693b2b185d354e08a58fa
Hiro <hirohiro11071107@gmail.com>
2016-11-05 15:07:14 +0900

    make seed timelines

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 use App\User;
+use App\Timelines;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('UserTableSeeder');
+        $this->call('TimelinesTableSeeder');
 
         Model::reguard();
     }
@@ -44,6 +46,62 @@ class UserTableSeeder extends Seeder
 			"name"=>"hanabusa",
 			"email"=>"hanabusa@mail.com",
 			"password"=>$password_hanabusa
+		]);
+	}
+
+}
+
+class TimelinesTableSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('Timelines')->delete();
+		Timelines::create([
+			"content_id"=>1,
+			"title"=>"世界大会予選で負けました",
+			"message"=>"いつもお世話になっています。最近ドリフトの世界大会に行ってきました。
+                        予選で負けてしまいましたが、これからも応援お願いします。",
+            "picture1"=>"/img/IAN2.JPG",
+            "picture2"=>"/img/IAN2.JPG",
+            "picture3"=>"/img/IAN2.JPG",
+            "picture4"=>"/img/IAN2.JPG",
+            "picture5"=>"/img/IAN2.JPG",
+            "video"=>"/img/IAN2.JPG"
+		]);
+        Timelines::create([
+			"content_id"=>1,
+			"title"=>"サポーターがつきました！",
+			"message"=>"いつもお世話になっています。
+                        最近ACAという会社がサポーターになってくれました。
+                        これからも応援お願いします。",
+            "picture1"=>"/img/IAN3.JPG",
+            "picture2"=>"/img/IAN3.JPG",
+            "picture3"=>"/img/IAN3.JPG",
+            "picture4"=>"/img/IAN3.JPG",
+            "picture5"=>"/img/IAN3.JPG",
+            "video"=>"/img/IAN3.JPG"
+		]);
+        Timelines::create([
+			"content_id"=>2,
+			"title"=>"あああああああ",
+			"message"=>"あああ",
+            "picture1"=>"/img/IAN.JPG",
+            "picture2"=>"/img/IAN2.JPG",
+            "picture3"=>"/img/IAN3.JPG",
+            "picture4"=>"/img/IAN.JPG",
+            "picture5"=>"/img/IAN2.JPG",
+            "video"=>"/img/IAN3.JPG"
+		]);
+        Timelines::create([
+			"content_id"=>3,
+			"title"=>"あああああああ",
+			"message"=>"あああ",
+            "picture1"=>"/img/IAN.JPG",
+            "picture2"=>"/img/IAN.JPG",
+            "picture3"=>"/img/IAN.JPG",
+            "picture4"=>"/img/IAN.JPG",
+            "picture5"=>"/img/IAN.JPG",
+            "video"=>"/img/IAN.JPG"
 		]);
 	}
 
